3781811a7187c6ec07f03234415830c8a4a3f422
Fernando Saraiva <fernandotiweb@gmail.com>
2021-05-05 22:07:52 -0300

    Melhorando layout e estilizando css

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 
 var gulp = require ('gulp');
 var sass = require ('gulp-sass');
+var autoprefixer = require ('gulp-autoprefixer');
 
 function compilaSass(){
     return gulp
     .src('css/scss/*.scss')
-    .pipe(sass())
+    .pipe(sass({outputStyle:'compressed'}))
+    .pipe(autoprefixer({
+        cascade: false
+    }))
     .pipe(gulp.dest('css/'))
 }
 
 gulp.task('sass', compilaSass);
+
+function watch() {
+    gulp.watch('css/scss/*.scss' , compilaSass)
+}
+
+gulp.task('default', watch);
