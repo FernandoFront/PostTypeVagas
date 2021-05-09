5eaadb03dbdf9e05fe255865c87dfa52cdcb3282
Fernando Saraiva <38087397+FernandoFront@users.noreply.github.com>
2021-05-09 19:23:44 -0300

    search filter

--- page-home.php
+++ page-home.php
@@ -3,7 +3,7 @@
 get_header();
 ?>
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                <div class="main_content"  uk-filter="target: .js-filter; animation: fade">
+                <div class="main_content">
                 <!--Filtro lateral-->
                 <asside class="filtro"> 
                     <ul class="uk-flex uk-flex-column uk-text-center uk-subnav uk-subnav-pill">
@@ -18,9 +18,9 @@ get_header();
                 <div class="container">
 
                     <!--Form pesquisa-->
-                    <form class="search uk-search uk-search-default">
+                    <form class="search uk-search uk-search-default" action="<?php bloginfo('url'); ?>/Vaga/" method="get">
                         <span uk-search-icon></span>
-                        <input class="uk-search-input" type="search" placeholder="Pesquisar vaga...">
+                        <input class="uk-search-input" type="search" name="s" id="s" value="<?php the_search_query() ?>" placeholder="Pesquisar vaga...">
                     </form>
                     <!--Fim Form pesquisa-->
 
@@ -40,21 +40,26 @@ get_header();
                    
                         <ul class="uk-flex uk-margin">
                             
-                            <?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts()) : $the_query->the_post(); ?>
+                           
                             <li class="uk-margin uk-flex-column">
-                                    <?php include(TEMPLATEPATH . "/inc/vaga.php"); ?> 
+                                <?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts()) : $the_query->the_post(); ?>
+                                    <?php include(TEMPLATEPATH . "/inc/vaga.php"); ?>
+                                <?php endwhile; else: endif; ?> 
                             </li> 
-                            <?php endwhile; else: endif; ?> 
+                            
+
+                            <?php wp_reset_query(); wp_reset_postdata(); ?> 
                         </ul>  
 
+                    <!-- Paginação -->
                     <?php
-                echo paginate_links (array(
+                    echo paginate_links (array(
                     'current' => max(1, get_query_var('paged') ),
                     'total' => $the_query->max_num_pages,
                     'prev_text' => __(""),
                     'next_text' => __("")
-                ));
-            ?>
+                        ));
+                    ?>
                     <!--Fim Toogle vaga-->    
                 </div>
 
